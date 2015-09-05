b1cf0757bfe6711d0531fd48aa60745bbfe06f15
braindumb <qiaobrandon@gmail.com>
2015-09-05 00:59:59 -0400

    edit 3 (Fixed Big Problem)

--- js/reader/read.js
+++ js/reader/read.js
@@ -1,5 +1,5 @@
 
-function speak(speech, rate){
+function speak(){
 meSpeak.loadConfig("mespeak_config.json");
 meSpeak.loadVoice("voices/en/en-us.json");
 meSpeak.speak(this.speech, {speed: rate});
--- js/reader/runtime.js
+++ js/reader/runtime.js
@@ -2,7 +2,10 @@ var speech = document.getElementById("userSpeech").value;
 
 function submit()
 {
-
 	document.getElementById("userSpeech").innerHTML = speech;
 	document.getElementById("wordCount").innerHTML = "Words: "+wordCount;
+}
+
+function read(){
+	readContent(speech, rate);
 }
